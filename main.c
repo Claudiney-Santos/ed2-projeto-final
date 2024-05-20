6c0079a118b2db9fec4640ea2f1627195f042827
Claudiney-Santos <claudineysantos@alunos.utfpr.edu.br>
2024-05-20 14:43:35 -0300

    Pequena modificação no arquivo main.c para testes da nova biblioteca de processos

--- main.c
+++ main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "processos.h"
+
+void chomp(char* str) {
+    str[strcspn(str,"\n")]=0;
+}
 
 int main() {
-    printf("Ola, Mundo!\n");
+    int codigo=1, prioridade;
+    statusCodigo status=pronto;
+    char* nome=(char*)malloc(21*sizeof(char));
+
+    printf("Informe o nome do processo: ");
+    fgets(nome, 21, stdin);
+    chomp(nome);
+
+    printf("Informa a prioridade: ");
+    scanf("%d", &prioridade);
+
+    processo* p=novoProcesso(codigo, nome, prioridade, status);
+
+    printf("PROCESSOS:\nCodigo: %d\nNome: %s\nPrioridade: %d\nStatus: %d\n", p->codigo, p->nome, p->prioridade, p->status);
+
+    encerrarProcesso(&p);
+
     return 0;
 }

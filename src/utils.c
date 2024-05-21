a594443393194c5425d70c61588176991af1a5a0
Claudiney-Santos <claudineysantos@alunos.utfpr.edu.br>
2024-05-21 18:17:04 -0300

    Adicionadas funções para utils

--- src/utils.c
+++ src/utils.c
@@ -1,8 +1,62 @@
 #include "utils.h"
 #include <string.h>
 
+void upperString(char* str) {
+    if(!str)
+        return;
+    int i;
+    for(i=0;str[i];i++)
+        if(str[i]>='a'&&str[i]<='z')
+            str[i]-=32;
+}
+
+void lowerString(char* str) {
+    if(!str)
+        return;
+    int i;
+    for(i=0;str[i];i++)
+        if(str[i]>='A'&&str[i]<='Z')
+            str[i]+=32;
+}
+
 void chomp(char* str) {
+    if(!str)
+        return;
     str[strcspn(str,"\n")]=0;
+}
+
+char isNumeric(char* str) {
+    if(!str)
+        return 0;
+    int i;
+    for(i=0;str[i];i++)
+        if(str[i]<'0'||str[i]>'9')
+            return 0;
+    return 1;
+}
+
+char isAlphabetic(char* str) {
+    if(!str)
+        return 0;
+    int i;
+    for(i=0;str[i];i++)
+        if(str[i]<'A'||str[i]>'z'||(str[i]>'Z'&&str[i]<'a'))
+            return 0;
+    return 1;
+}
+
+char isAlphanumeric(char* str) {
+    if(!str)
+        return 0;
+    int i;
+    for(i=0;str[i];i++)
+        if(!(
+            (str[i]>='0'&&str[i]<='9') ||
+            (str[i]>='A'&&str[i]<='Z') ||
+            (str[i]>='a'&&str[i]<='z')
+            ))
+            return 0;
+    return 1;
 }
 
 int mod(int a, int b) {
--- src/utils.h
+++ src/utils.h
@@ -1,7 +1,14 @@
 #ifndef _UTILS_H_
 #define _UTILS_H_
 
+void upperString(char* str);
+void lowerString(char* str);
+
 void chomp(char* str);
+char isNumeric(char* str);
+char isAlphabetic(char* str);
+char isAlphanumeric(char* str);
+
 int mod(int a, int b);
 
 #endif

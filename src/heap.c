eb36e17d19328dc2205a4f8872a5932a0a67e771
Claudiney-Santos <claudineysantos@alunos.utfpr.edu.br>
2024-05-24 18:55:13 -0300

    Inicio da implementação de heap

--- src/heap.c
+++ src/heap.c
@@ -1,1 +1,20 @@
 #include "heap.h"
+
+heap novoHeap(size_t tamanhoInicial, int(*extraiPrioridade)(void*)) {
+    if(!extraiPrioridade)
+        return NULL;
+    heap* h=(heap*)malloc(sizeof(heap));
+    if(h) {
+        h->raiz=-1;
+        h->tamanho=tamanhoInicial;
+        h->extraiPrioridade=extraiPrioridade;
+        h->vet=(void**)malloc(sizeof(void*));
+    }
+    return h;
+}
+
+int insereHeap(heap* h, void* val) {
+    
+}
+void* removeMaximo(heap* h);
+int heapifica(heap* h);
--- src/heap.h
+++ src/heap.h
@@ -1,6 +1,18 @@
 #ifndef _HEAP_H_
 #define _HEAP_H_
 
+#include <stdlib.h>
 
+typedef struct {
+    void** vet;
+    int raiz;
+    size_t tamanho;
+    int(*extraiPrioridade)(void*);
+} heap;
+
+heap novoHeap(size_t tamanhoInicial, int(*extraiPrioridade)(void*));
+int insereHeap(heap* h, void* val);
+void* removeMaximo(heap* h);
+int heapifica(heap* h);
 
 #endif

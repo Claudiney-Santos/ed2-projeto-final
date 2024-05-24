b26959139a3afdb118655e6158156c2f11db74a8
Claudiney-Santos <claudineysantos@alunos.utfpr.edu.br>
2024-05-24 17:45:08 -0300

    Iniciado o desenvolvimento do processo de teste do parser

--- src/main.c
+++ src/main.c
@@ -1,127 +1,82 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "utils.h"
-#include "processos.h"
+#include "parser.h"
 #include "lista.h"
 
-void iniciaProcesso(lista* l);
-void veProcesso(lista* l);
-void veProcessos(lista* l);
-void removeProcesso(lista* l);
+void mostrarComandos(lista* l);
 
-void encerraProcessoLista(void* val) {
-    processo* p=(processo*)val;
-    encerraProcesso(&p);
+int main(int argc, char** argv) {
+    char rodar=1;
+    int escolha=1;
+    FILE* arquivo=NULL;
+    lista* l=NULL;
+
+    l=novaLista();
+    while(rodar) {
+        printf("");
+    }
+    return 0;
 }
 
-int main() {
-    lista* l=novaLista();
-    if(!l)
-        return 1;
-
-    int escolha=-1;
-    while(escolha) {
-        printf("1) Iniciar processo\n2) Ver processo\n3) Ver processos\n4) Remover processo\n0) Sair\n\nEscolha: ");
-        scanf("%d", &escolha);
-        printf("\n");
-        switch(escolha) {
-            case 1:
-                iniciaProcesso(l);
+void mostrarComandos(lista* l) {
+    if(!l||!l->raiz) {
+        printf("Nao ha comandos salvos!\n");
+        return;
+    }
+    no* n=NULL;
+    int i=0;
+    for(n=l->raiz;n;n=n->prox, i++) {
+        token* t=(token*)n->val;
+        if(i)
+            printf("===\n");
+        printf("Comando %d: ", i);
+        switch(t->cmd) {
+            case iniciar:
+                printf("iniciar");
                 break;
-            case 2:
-                veProcesso(l);
+            case encerrar:
+                printf("encerrar");
                 break;
-            case 3:
-                veProcessos(l);
+            case inserirAvl:
+                printf("inserirAVL");
                 break;
-            case 4:
-                removeProcesso(l);
+            case terminarAvl:
+                printf("terminarAVL");
                 break;
-            case 0:
+            case listarAvl:
+                printf("listarAVL");
                 break;
-            default:
-                printf("Escolha invalida!\n");
+            case alterarHeap:
+                printf("alterarHeap");
+                break;
+            case removerHeap:
+                printf("removerHeap");
+                break;
+            case listarHeap:
+                printf("listarHeap");
+                break;
+            case bloquearHash:
+                printf("bloquearHash");
+                break;
+            case desbloquearHash:
+                printf("desbloquearHash");
+                break;
+            case executar:
+                printf("executar");
+                break;
+            case removerHash:
+                printf("removerHash");
+                break;
+            case listarHash:
+                printf("listarHash");
+                break;
+            case terminar:
+                printf("terminar");
+                break;
         }
         printf("\n");
     }
 
-    liberaListaFunc(&l, encerraProcessoLista);
-
-
-    return 0;
 }
 
-void iniciaProcesso(lista* l) {
-    if(!l)
-        return;
-    int codigo=0, prioridade=0, status=0;
-    char* nome=(char*)malloc(21*sizeof(char));
-
-    printf("Informe o codigo: ");
-    scanf("%d", &codigo);
-
-    printf("Informe o nome: ");
-    fgets(nome, 21, stdin);
-    chomp(nome);
-
-    printf("Informe a prioridade: ");
-    scanf("%d", &prioridade);
-
-    do {
-        printf("Status:\n1) PRONTO\n2) EXECUCAO\n3) BLOQUEADO\n\nInforme: ");
-        scanf("%d", &status);
-    } while(status<0||status>3);
-
-    insereUltimo(l,
-        (void*)novoProcesso(codigo, nome, prioridade, (statusCodigo)status));
-}
-
-void veProcesso(lista* l) {
-    if(!l)
-        return;
-    int indice=0;
-    processo* p=NULL;
-    printf("Informe o indice: ");
-    scanf("%d", &indice);
-    p=(processo*)pegaPosicao(l, indice);
-    printf("\n");
-    if(p)
-        printf("Processo:\nCodigo: %d\nNome: \"%s\"\nPrioridade: %d\nStatus: %d\n", p->codigo, p->nome, p->prioridade, p->status);
-    else
-        printf("Processo nao encontrado!\n");
-    printf("\n");
-}
-
-void mostraProcessos(void* val, int indice, lista* _) {
-    processo* p=(processo*)val;
-    if(indice)
-        printf("===\n");
-    printf("Processo %d:\n", indice);
-    printf("Codigo: %d\nNome: \"%s\"\nPrioridade: %d\nStatus: %d\n", p->codigo, p->nome, p->prioridade, p->status);
-}
-
-void veProcessos(lista* l) {
-    if(!l)
-        return;
-    paraCada(l, mostraProcessos);
-    printf("\n");
-}
-
-void removeProcesso(lista* l) {
-    if(!l)
-        return;
-    int indice=0;
-    processo* p=NULL;
-    printf("Informe o indice: ");
-    scanf("%d", &indice);
-
-    p=(processo*)removePosicao(l, indice);
-
-    if(p) {
-        printf("Removido o processo %d:\n", indice);
-        printf("Codigo: %d\nNome: \"%s\"\nPrioridade: %d\nStatus: %d\n", p->codigo, p->nome, p->prioridade, p->status);
-        encerraProcesso(&p);
-    } else
-        printf("Nao foi possivel remover esse processo\n");
-}

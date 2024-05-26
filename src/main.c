05c2decf9ada81db77a34daed36e2464162a98f9
Claudiney-Santos <claudineysantos@alunos.utfpr.edu.br>
2024-05-26 12:46:05 -0300

    Continuada a implementacao do teste para o parser, ainda nao finalizada

--- src/main.c
+++ src/main.c
@@ -1,28 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#include "parser.h"
 #include "lista.h"
+#include "parser.h"
 
-void mostrarComandos(lista* l);
+#define INPUT_TAMANHO 50
+
+int mostraComandos(lista* l);
+int carregaScript(FILE** arquivo, lista** l);
+
+void encerraProcessoLista(void* val) {
+    processo* p=(processo*)val;
+    encecerraProcesso(&p);
+}
 
 int main(int argc, char** argv) {
-    char rodar=1;
-    int escolha=1;
+    long int escolha=1;
+    int err=0;
+    char input[INPUT_TAMANHO];
+    char nomeArquivo[INPUT_TAMANHO];
+    char* saida;
     FILE* arquivo=NULL;
     lista* l=NULL;
+    char rodar=1;
 
-    l=novaLista();
-    while(rodar) {
-        printf("");
+    while(rodar&&!err) {
+        if(arquivo)
+            printf("Arquivo carregado: \"%s\"\n", nomeArquivo);
+        else
+            printf("Nenhum arquivo carregado\n");
+        printf("===\n")
+        printf("1) Iniciar simulacao\n2) Ver script carregado\n3) Carregar script\n0) Sair\n\nEscolha: ");
+        fgets(input, INPUT_TAMANHO, stdin);
+        escolha=strtol(input, &saida, 10);
+        if(!strcmp(input,saida))
+            continue;
+        switch(escolha) {
+            case 1:
+                break;
+            case 2:
+                err=mostraComandos(l);
+                break;
+            case 3:
+                err=carregaScript(&arquivo, &l);
+        }
     }
     return 0;
 }
 
-void mostrarComandos(lista* l) {
+int mostraComandos(lista* l) {
     if(!l||!l->raiz) {
         printf("Nao ha comandos salvos!\n");
-        return;
+        return 0;
     }
     no* n=NULL;
     int i=0;
@@ -77,6 +107,31 @@ void mostrarComandos(lista* l) {
         }
         printf("\n");
     }
+    return 0;
+}
 
+int carregaScript(FILE** arquivo, lista** l) {
+    char input[INPUT_TAMANHO];
+    if(!arquivo||!l)
+        return 1;
+    else if(*arquivo)
+        fclose(*arquivo);
+    if(*l)
+        liberaListaFunc(l, encerraProcessoLista);
+    printf("Informe o nome do arquivo: ");
+    fgets(input, INPUT_TAMANHO, stdin);
+    chomp(input);
+    *arquivo=fopen(input, "r");
+    if(!(*arquivo)) {
+        printf("Nao foi possivel abrir o arquivo \"%s\"\n", input);
+        return 2;
+    }
+    *l=tokenizaLista(*arquivo);
+    if(!l) {
+        fclose(*arquivo);
+        *arquivo=NULL;
+        return 3;
+    }
+    return 0;
 }
 

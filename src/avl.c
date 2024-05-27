55a273e27fb8f4d14a3456f6ed55ee0608f774cc
Claudiney-Santos <claudineysantos@alunos.utfpr.edu.br>
2024-05-27 14:57:19 -0300

    Iniciada a implementação da avl

--- src/avl.c
+++ src/avl.c
@@ -1,1 +1,73 @@
 #include "avl.h"
+
+noAvl* novoNoAvl(void* val) {
+    noAvl* n=(noAvl*)malloc(sizeof(noAvl));
+    if(n) {
+        n->val=val;
+        n->altura=0;
+        n->esq=n->dir=NULL;
+    }
+    return n;
+}
+
+void* liberaNoAvl(noAvl** n) {
+    if(!n||!(*n))
+        return NULL;
+    void* val=(*n)->val;
+    free(*n);
+    *n=NULL;
+    return val;
+}
+
+int insereAvlNo(noAvl** n, void* val, int(*extraiChave)(void*)) {
+    if(!n)
+        return -1;
+    else if(!(*n)) {
+        *n=novoNoAvl(val);
+        return 0;
+    }
+    int chave=extraiChave(val), chaveN=extraiChave(*n);
+    // IMPLEMENTAÇÃO INCOMPLETA
+}
+
+avl* novaAvl(int (*extraiChave)(void*)) {
+    avl* a=NULL;
+    if(extraiChave)
+        a=(avl*)malloc(sizeof(avl));
+    if(a) {
+        a->raiz=NULL;
+        a->extraiChave=extraiChave;
+    }
+    return a;
+}
+
+void liberaAvl(avl** a) {
+    free(*a);
+    *a=NULL;
+}
+
+int insereAvl(avl* a, void* val) {
+    if(!a)
+        return -1;
+    else if(!a->raiz) {
+        a->raiz=novoNoAvl(val);
+        return 0;
+    }
+    int chave=extraiChave(val), chaveN;
+    noAvl* l=NULL;
+    noAvl* m=NULL;
+    noAvl** n=&(a->raiz);
+    while(*n) {
+        chaveN=a->extraiChave(*n);
+        if(chave==chaveN)
+            return 1;
+        l=m;
+        m=n;
+        n=chave<chaveN ? &((*n)->esq) : &((*n)->dir);
+    }
+    *n=novoNoAvl(val);
+}
+
+void* pegaAvl(avl* a, int chave);
+void* removeAvl(avl* a, int chave);
+void listaAvl(avl* a);
--- src/avl.h
+++ src/avl.h
@@ -1,6 +1,30 @@
 #ifndef _AVL_H_
 #define _AVL_H_
 
+typedef struct NoAvl {
+    void* val;
+    int altura;
+    struct NoAvl esq;
+    struct NoAvl dir;
+} noAvl;
+
+typedef struct {
+    noAvl* raiz;
+    int (*extraiChave)(void*);
+} avl;
+
+noAvl* novoNoAvl(void* val);
+void* liberaNoAvl(noAvl** n);
+
+int insereAvlNo(noAvl** n, void* val, int(*extraiChave)(void*));
+
+avl* novaAvl(int (*extraiChave)(void*));
+void liberaAvl(avl** a);
+
+int insereAvl(avl* a, void* val);
+void* pegaAvl(avl* a, int chave);
+void* removeAvl(avl* a, int chave);
+void listaAvl(avl* a);
 
 
 #endif
